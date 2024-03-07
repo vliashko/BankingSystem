2095f6812d759a5948980d2ee610211705d94eb7
Vladzimir Liashko <vladzimir.liashko@gmail.com>
2024-03-07 14:30:15 +0100

    xxxxxxxxxxxx

---
+++ BankingSystem.API/Logic/SImpleLogicClass.cs
@@ -0,0 +1,17 @@
+﻿namespace BankingSystem.API.Logic
+{
+	public static class SImpleLogicClass
+	{
+		public static int Divide(int x, int y)
+		{
+			if (y == 0)
+			{
+				return 0;
+			}
+			else
+			{
+				return x / y;
+			}
+		}
+	}
+}
