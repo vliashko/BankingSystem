64526f6f01e55023bf68d7cd134ddb8bf07b8f24
26ledi <joyceledi26@gmail.com>
2024-03-08 18:09:47 +0300

    Added Basic Middlewares

---
+++ BankingSystem.API/Extensions/ApplicationDependanciesConfiguration.cs
@@ -0,0 +1,13 @@
+﻿namespace BankingSystem.API.Extensions
+{
+    public static class ApplicationDependanciesConfiguration
+    {
+        public static IServiceCollection ConfigureServices(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddCors();
+       
+            return builder.Services;
+        }
+    }
+}
