0c1635673b71ce21f98f835ccaa9ab857e39237d
26ledi <joyceledi26@gmail.com>
2024-03-12 16:26:40 +0300

    Added EF dependancies Added DataAccess Layer Configured Database

---
+++ BankingSystem.DataAccess/Data/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BankingSystem.DataAccess.Data
+{
+    public class Context:DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder builder) 
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
