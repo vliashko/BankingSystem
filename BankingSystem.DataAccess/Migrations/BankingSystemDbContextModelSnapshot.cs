5d380c2bd236d23034dfe691d8a3863326915871
26ledi <joyceledi26@gmail.com>
2024-04-11 17:23:09 +0300

    Removed Uneccessary files

--- BankingSystem.DataAccess/Migrations/BankingSystemDbContextModelSnapshot.cs
+++ BankingSystem.DataAccess/Migrations/BankingSystemDbContextModelSnapshot.cs
@@ -10,7 +10,6 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BankingSystem.DataAccess.Migrations
 {
-    [ExcludeFromCodeCoverage]
     [DbContext(typeof(BankingSystemDbContext))]
     partial class BankingSystemDbContextModelSnapshot : ModelSnapshot
     {
