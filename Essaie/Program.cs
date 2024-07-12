ad4265b1e7de1cedbb3124cc8e7885fb7091a92d
26ledi <joyceledi26@gmail.com>
2024-07-12 12:31:30 +0300

    Added Azure Functions

---
+++ Essaie/Program.cs
@@ -0,0 +1,12 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+channel.QueueDeclare(queue: "test-queue", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+var body = Encoding.UTF8.GetBytes("Hello RabbitMQ!");
+channel.BasicPublish(exchange: "", routingKey: "bankingsystem-queue", basicProperties: null, body: body);
+
+Console.WriteLine(" [x] Sent 'Hello RabbitMQ!'");
